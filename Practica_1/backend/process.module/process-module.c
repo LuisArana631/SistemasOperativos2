1e88190a14427e0bb626dacb17adc0975787c6f8
Luis <luarana631@gmail.com>
2021-09-05 14:40:21 -0600

    Process_module

--- Practica_1/backend/process.module/process-module.c
+++ Practica_1/backend/process.module/process-module.c
@@ -60,33 +60,30 @@ static int my_proc_show(struct seq_file *m, void *v)
         struct task_struct *task_child;
         struct list_head *list; 
         unsigned long rss;
-        unsigned long size_code;
 
         /* Recorrer a grandes rasgos los procesos */
 		seq_printf(m, "{\"procesos\": [\n");
         for_each_process(task) {
             get_task_struct(task);
 
-            size_code = task->mm->start_code - task->mm->end_code;
             /* Extraer memoria utiliza en bytes */
             if (task->mm) {
                 rss = get_mm_rss(task->mm) << PAGE_SHIFT;
-                seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%lu\", \"codeSize\":\"%lu\"},\n",task->comm , task->pid, task->state, task->parent->pid, task->recent_used_cpu, rss, size_code);
+                seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%lu\", \"codeSize\":\"%lu\"},\n",task->comm , task->pid, task->state, task->parent->pid, task->recent_used_cpu, rss, task->mm->start_code);
             }else{
-                seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%d\", \"codeSize\":\"%lu\"},\n",task->comm , task->pid, task->state, task->parent->pid, task->recent_used_cpu, 0, size_code);
+                seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%d\", \"codeSize\":\"%lu\"},\n",task->comm , task->pid, task->state, task->parent->pid, task->recent_used_cpu, 0, task->mm->start_code);
             }
 
             /* Recorrer los procesos hijos */
             list_for_each(list, &task->children){
                 task_child = list_entry( list, struct task_struct, sibling );
                 get_task_struct(task_child);
-
-                size_code = task_child->mm->start_code - task_child->mm->end_code;
+                
                 if (task_child->mm) {
                     rss = get_mm_rss(task_child->mm) << PAGE_SHIFT;
-                    seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%lu\", \"codeSize\":\"%lu\"},\n",task_child->comm , task_child->pid, task_child->state, task_child->parent->pid, task_child->recent_used_cpu, rss, size_code);
+                    seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%lu\", \"codeSize\":\"%lu\"},\n",task_child->comm , task_child->pid, task_child->state, task_child->parent->pid, task_child->recent_used_cpu, rss, task_child->mm->start_code);
                 }else{
-                    seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%d\", \"codeSize\":\"%lu\"},\n",task_child->comm , task_child->pid, task_child->state, task_child->parent->pid, task_child->recent_used_cpu, 0, size_code);
+                    seq_printf(m, "{\"name\": \"%s\", \"pid\":%d, \"state\":%lu, \"father\":%d, \"usedCpu\": \"%d\", \"usedRAM\": \"%d\", \"codeSize\":\"%lu\"},\n",task_child->comm , task_child->pid, task_child->state, task_child->parent->pid, task_child->recent_used_cpu, 0, task_child->mm->start_code);
                 }   
 
                 put_task_struct(task_child);            
