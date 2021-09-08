7b5f15276b986aac55e529aef8cded2f91fd61e7
Diana Menendez <dcmc2297@gmail.com>
2021-09-08 17:55:30 -0600

    Administrador de procesos terminado

--- Practica_1/frontend/src/views/Admin_Procesos.js
+++ Practica_1/frontend/src/views/Admin_Procesos.js
@@ -13,18 +13,69 @@ import {
 import PanelHeader from "components/PanelHeader/PanelHeader.js";
 import { get_proc } from 'services/services.js'
 
+var tprocesos=0;
+var tejecucion=0;
+var tsuspendidos=0;
+var tdetenidos=0;
+var tzombie=0;
+
+function calcular(process_data){
+  //let process_data  = get_process_data(get_proc());
+  tprocesos=process_data.length;
+  for(let i=0; i<process_data.length;i++){
+    if(process_data[i].state == "suspended"){
+      tsuspendidos++;
+    }else if(process_data[i].state == "stopped"){
+      tdetenidos++;
+    }else if(process_data[i].state == "zombie"){
+      tzombie++;
+    }else if(process_data[i].state == "running"){
+      tejecucion++;
+    }
+  }
+}
+
 function Admin_Procesos() {
-
-  let process_data  = get_process_data(get_proc());
-
+  var process_data  = get_process_data(get_proc());
+  calcular(process_data);
   return (
     <>
       <PanelHeader size="sm" />
       <div className="content">
+      <Row>
+          <Card>
+              <CardHeader>
+                <CardTitle tag="h4">Administrador de procesos</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Table responsive>
+                  <thead className="text-primary">
+                    <tr>
+                      <th> <b>Total de procesos</b></th>
+                      <th> <b>Procesos en ejecución</b></th>
+                      <th> <b>Procesos suspendidos</b></th>
+                      <th> <b>Procesos detenidos</b></th>
+                      <th> <b>Procesos zombies</b></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{tprocesos}</td>
+                      <td>{tejecucion}</td>
+                      <td>{tsuspendidos}</td>
+                      <td>{tdetenidos}</td>
+                      <td>{tzombie}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          
+        </Row>
         <Row>
           <Card>
               <CardHeader>
-                <CardTitle tag="h4">Lista de procesos</CardTitle>
+                <CardTitle tag="h4">Arbol de procesos</CardTitle>
               </CardHeader>
               <CardBody>
                 <Table responsive>
@@ -71,7 +122,8 @@ function get_process_data(process_array){
   let array_return = [];
     
   if (process_array != undefined) {
-    Array.from(process_array.procesos).map(row =>  {
+    if(process_array.procesos != undefined){
+      Array.from(process_array.procesos).map(row =>  {
         let estado_string = row.state == 0 ? "running" : row.state == 1 ? "stopped" : "zombie" ;
 
         array_return.push({
@@ -83,9 +135,11 @@ function get_process_data(process_array){
           "usedCpu": row.usedCpu,
           "usedRAM": row.usedRAM,
           "usuario": row.usuario
-      })
+        })
       }      
-    );
+      );
+    }
+    
   }  
 
   return array_return;
